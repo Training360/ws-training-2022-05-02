0cb12584e63b1e46eb3bc29e190df7fa3057e1f0
Istvan Viczian <viczian.istvan@gmail.com>
2022-05-03 12:00:40 +0200

    Namespace and validation JAXB

--- xml-demo/src/main/java/jaxb/JaxbReaderMain.java
+++ xml-demo/src/main/java/jaxb/JaxbReaderMain.java
@@ -1,6 +1,9 @@
 package jaxb;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
 
 public class JaxbReaderMain {
 
@@ -9,8 +12,12 @@ public class JaxbReaderMain {
             var context = JAXBContext.newInstance(Catalog.class, Book.class);
             var unmarshaller = context.createUnmarshaller();
 
+            var factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            var schema = factory.newSchema(new StreamSource(JaxbReaderMain.class.getResourceAsStream("/catalog.xsd")));
+            unmarshaller.setSchema(schema);
+
             Catalog catalog = (Catalog)
-                    unmarshaller.unmarshal(JaxbReaderMain.class.getResourceAsStream("/catalog.xml"));
+                    unmarshaller.unmarshal(JaxbReaderMain.class.getResourceAsStream("/catalogns.xml"));
 
             for (Book book: catalog.getBooks()) {
                 System.out.println(book.getIsbn10());
