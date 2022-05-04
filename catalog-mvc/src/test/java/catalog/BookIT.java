461e984759c39dcdd346207d0969bac051691991
Istvan Viczian <viczian.istvan@gmail.com>
2022-05-04 20:42:33 +0200

    Rossz tesztesetek

--- catalog-mvc/src/test/java/catalog/BookIT.java
+++ catalog-mvc/src/test/java/catalog/BookIT.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,32 +21,75 @@ class BookIT {
 
     @Test
     void testCreateTwoBooksThanListThem() {
+        // Összes könyv törlése
+        client
+                .delete()
+                .uri("/api/books")
+                .exchange()
+                .expectStatus().isNoContent();
+
+        // Első könyv létrehozása
         client
                 .post()
                 .uri("/api/books")
-                .bodyValue(new Book("aaa", "Test"))
+                .bodyValue(new Book("aaa", "Test 1"))
                 .exchange()
                 .expectStatus().isCreated();
 
+        // Második könyv létrehozása
         client
                 .post()
                 .uri("/api/books")
-                .bodyValue(new Book("bbb", "Test"))
+                .bodyValue(new Book("bbb", "Test 2"))
                 .exchange()
-                .expectStatus().isCreated()
+                .expectStatus().isCreated() // status ellenőrzése
                 .expectBody(Book.class)
-                .value(b -> assertEquals("bbb" ,b.getTitle()));
+                .isEqualTo(new Book("bbb", "Test 2")) // konkrét objektum példánnyal összehasonlítás
+                .value(b -> assertEquals("Test 2", b.getTitle())); // assert használata
 
-        // List<Book>.class
+        // Összes könyv lekérdezése
+        client
+                .get()
+                .uri("/api/books")
+                .exchange()
+                .expectBody().
+                jsonPath("$[1].title").isEqualTo("Test 2"); // Ellenőrzés JSON PATH-szal
 
-//        ParameterizedTypeReference<List<Book>> typeReference = new ParameterizedTypeReference<List<Book>>(){};
-//        client
-//                .get().uri("/api/books")
-//
-//                .exchange()
-//
-//                .expectBodyList(typeReference).hasSize(2).contains(
-//                        List.of(new Book("aaa", "Test"), new Book("bbb", "Test"))
-//                );
+        client
+                .get()
+                .uri("/api/books")
+                .exchange()
+                .expectBodyList(Book.class)// Ellenőrzés, támaszkodva arra, hogy egy lista
+                .value(books -> System.out.println(books)) // Debugolásképp kiírva
+                .hasSize(2)
+                .contains(new Book("aaa", "Test 1"), new Book("bbb", "Test 2"));
+
+        var typeReference = new ParameterizedTypeReference<List<Book>>() {
+        };
+        client
+                .get()
+                .uri("/api/books")
+                .exchange()
+                .expectBody(typeReference) // Ellenőrzés, a visszatérési értéket csak objektumként kezelve
+                .value(books -> assertEquals(2, books.size()));
+
+        // Objektum direkt lekérése, hogy külön assert-eket lehessen rajta végrehajtani
+
+        var books = client
+                .get()
+                .uri("/api/books")
+                .exchange()
+                .expectBody(typeReference)
+                .returnResult().getResponseBody();
+        assertEquals(2, books.size());
+
+        // Válasz json kiírása a konzolra
+        var json = client
+                .get()
+                .uri("/api/books")
+                .exchange()
+                .expectBody()
+                .returnResult().getResponseBody();
+        System.out.println(new String(json, StandardCharsets.UTF_8));
     }
 }
